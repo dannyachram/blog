109560ac391a9c98bc060f26a30e055b694815a7
Danny Achramm <danny.achram@gmail.com>
2021-12-24 01:20:34 +0700

    merubah abotu file

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,10 +2,21 @@
 
 
 @section('container')
-    <H1>{{ $title }}</H1>
-    <h3>{{ $name }}</h3>
-    <p>{{ $email }}</p>
 
-    <img src="img/{{ $image }}" alt={{ $name }} width="200" class="img-thumbnail rounded-circle"> 
+    <div class="card justify-content-center mb-5 img-fluid">
+        {{-- <img src="/img/dannyachram.jpg" class="card-img-top" alt="..."> --}}
+        <H1>{{ $title }}</H1>
+        <h3>{{ $name }}</h3>
+        <p>{{ $email }}</p>
+
+        <img src="img/{{ $image }}" alt={{ $name }} width="200" class="img-thumbnail rounded-circle"> 
+          
+        </div>
+      </div>
+
+
+
+
+
 @endsection
 
