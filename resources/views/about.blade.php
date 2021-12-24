5bd54a5ddd013b0cf58a1e002d9ce3e44550dc7d
dannyachram <ufvp2tbo@178-18-253-214.cprapid.com>
2021-12-25 00:52:29 +0700

    perubahan style, htaccess, about

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -4,16 +4,11 @@
 @section('container')
 
     <div class="card justify-content-center mb-5 img-fluid">
-        {{-- <img src="/img/dannyachram.jpg" class="card-img-top" alt="..."> --}}
-        <H1>{{ $title }}</H1>
+        <H1 class="title-name justify-content-center">{{ $title }}</H1>
         <h3>{{ $name }}</h3>
         <p>{{ $email }}</p>
-
         <img src="img/{{ $image }}" alt={{ $name }} width="200" class="img-thumbnail rounded-circle"> 
-          
-        </div>
-      </div>
-
+    </div>
 
 
 
